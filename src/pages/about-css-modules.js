9f7b8ad6cb675c3a069f20926a23a2be2190c3f4
AGW <agiraldow@gmail.com>
2020-08-15 10:10:34 -0400

    Gradient version

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -5,28 +5,29 @@ import Container from "../components/container"
 export default function About() {
   return (
     <Container>
-      <h1>Ana Giraldo-Wingler</h1>
+      <h1>ANA G-W</h1>
       <p class="story">
           I product manage 📕, design 🎨, and build 🔧, sometimes all at the same time.
-      </p>
-      <p>
+        </p>
+        <p>
         From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend.
-      </p>
-      <p>
+        </p>
+        <p>
         During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>.
-      </p>
-      <p>
+        </p>
+        <p>
         Since 2017, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
-      </p>
-      <p>
-        2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
-      </p>
-      <p class="currently">
+        </p>
+        <p>
+        2020 has been a year of reflection for me, and I have realized that I really miss building things hands-on.
+        </p>
+        <p class="currently">
         I’m currently looking for a remote full-stack software engineering position focused on frontend. Let’s talk!
-      </p>
-      <p>
+        </p>
+        <p>
        <a href="mailto:agiraldow@gmail.com">agiraldow@gmail.com</a>
-      </p>
+        </p>
+        
 
     </Container>
   )
