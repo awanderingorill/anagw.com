dd67bb04149a2b2c97e1d6a0ca25453a643491bb
AGW <agiraldow@gmail.com>
2020-08-17 18:17:29 -0400

    Add gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,16 @@ import Container from "../components/container"
 export default function About() {
   return (
     <Container>
-      <h1>🌹Ana G-W</h1>
+      <div></div>
+      <h1>ANA G-W</h1>
       <p class="story">
-          I product manage, design, and build, sometimes all at the same time.
-      </p>
-      <p>
-        From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend.
-      </p>
-      <p>
-        During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>.
-      </p>
-      <p>
-        Since 2017, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
+          I product manage, design, and build, sometimes all at the same time. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2017, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p>
         2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
       </p>
       <p class="currently">
-        I’m currently looking for a remote full-stack software engineering position focused on frontend. Let’s talk!
+        I’m looking for a remote full-stack software engineering position focused on frontend. Let's talk! 🍝
       </p>
       <p>
        <a href="mailto:agiraldow@gmail.com">agiraldow@gmail.com</a>
