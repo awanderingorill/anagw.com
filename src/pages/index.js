e9a1694706550da340a47820b808f7e4f2094d7a
AGW <agiraldow@gmail.com>
2020-08-18 15:22:03 -0400

    Adjust margins and spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,9 @@ import Container from "../components/container"
 export default function About() {
   return (
     <Container>
-      <div class="gradient-circle"></div>
       <h1>Ana Giraldo-Wingler</h1>
       <p class="story">
-          I am an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
+          I'm an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p>
         2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
