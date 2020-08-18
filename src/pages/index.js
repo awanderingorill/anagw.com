9ebd3ecc6f4ac81aae1ff21c9b709f29a940e363
AGW <agiraldow@gmail.com>
2020-08-18 08:45:14 -0400

    Add more dimensions to gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ export default function About() {
   return (
     <Container>
       <div></div>
-      <h1>ANA G-W</h1>
+      <h1>Ana G-W</h1>
       <p class="story">
-          I product manage, design, and build, sometimes all at the same time. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2017, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
+          I product manage, design, and build, sometimes all at the same time. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p>
         2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
