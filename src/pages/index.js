2394c4d9fd53042252c46edfbbbfb0fa5d8d45e2
AGW <agiraldow@gmail.com>
2020-08-18 19:19:49 -0400

    Update media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,9 @@ export default function About() {
         Get in touch via <a href="mailto:agiraldow@gmail.com">email</a>. You can find me on <a href="http://twitter.com/awanderingorill" target="new">Twitter</a> and <a href="http://github.com/awanderingorill" target="new">GitHub</a>. I also post 🎵 on <a href="http://soundcloud.com/verita-gomez" target="new">SoundCloud</a>  and 📷 on <a href="http://instagram.com/anita.gw" target="new">Instagram</a>.
       </p>
       <p class="mushrooms">
+        <div class="mobile-description">Here is a photo I took of a cool mushroom, for good measure.</div>
         <img src="https://instagram.fewr1-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/117133712_3143536909055545_8441520914769024663_n.jpg?_nc_ht=instagram.fewr1-6.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qUA6uwe26fAAX8DOsGj&oh=e42e146c540624089cf6618960d57692&oe=5F675AAB"></img>
-        Here is a photo I took of a cool mushroom, for good measure.
+        <div class="description">Here is a photo I took of a cool mushroom, for good measure.</div>
       </p>
     </Container>
   )
