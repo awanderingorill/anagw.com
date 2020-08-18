23f9d4f9619431e5824243f2bcf78cf0a9d80675
AGW <agiraldow@gmail.com>
2020-08-18 14:30:59 -0400

    Remove gradient, left align

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import Container from "../components/container"
 export default function About() {
   return (
     <Container>
-      <div></div>
-      <h1>Ana G-W</h1>
+      <div class="gradient-circle"></div>
+      <h1>Ana Giraldo-Wingler</h1>
       <p class="story">
-          I product manage, design, and build, sometimes all at the same time. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
+          I am an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p>
         2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
@@ -19,7 +19,6 @@ export default function About() {
       <p>
        <a href="mailto:agiraldow@gmail.com">agiraldow@gmail.com</a>
       </p>
-
     </Container>
   )
 }
