9a442cfdd193740a0632705fec0a5e5bb610b1ad
AGW <agiraldow@gmail.com>
2020-08-22 18:53:59 -0400

    Change target new to target blank

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,13 @@ export default function About() {
         I’m currently looking for a remote full-stack software engineering position focused on frontend. Let's talk! 💫
       </p>
       <p class="contact">
-        Get in touch via <a href="mailto:agiraldow@gmail.com">email</a>. You can find me on <a href="http://twitter.com/awanderingorill" target="new">Twitter</a> and <a href="http://github.com/awanderingorill" target="new">GitHub</a>. I also post 🎵 on <a href="http://soundcloud.com/verita-gomez" target="new">SoundCloud</a>  and 📷 on <a href="http://instagram.com/anita.gw" target="new">Instagram</a>.
+        Get in touch via <a href="mailto:agiraldow@gmail.com">email</a>. You can find me on <a href="http://twitter.com/awanderingorill" target="_blank">Twitter</a> and <a href="http://github.com/awanderingorill" target="_blank">GitHub</a>. I also post 🎵 on <a href="http://soundcloud.com/verita-gomez" target="_blank">SoundCloud</a>  and 📷 on <a href="http://instagram.com/anita.gw" target="_blank">Instagram</a>.
       </p>
-      <p class="mushrooms">
+      {/* <p class="mushrooms">
         <div class="mobile-description">Here is a photo I took of a cool mushroom, for good measure.</div>
         <img src="https://instagram.fewr1-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/117133712_3143536909055545_8441520914769024663_n.jpg?_nc_ht=instagram.fewr1-6.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qUA6uwe26fAAX8DOsGj&oh=e42e146c540624089cf6618960d57692&oe=5F675AAB"></img>
         <div class="description">Here is a photo I took of a cool mushroom, for good measure.</div>
-      </p>
+      </p> */}
     </Container>
   )
 }
