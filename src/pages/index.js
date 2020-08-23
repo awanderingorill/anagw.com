29e7522d11b2ed70df085c6157608d0125aa08a0
AGW <agiraldow@gmail.com>
2020-08-22 21:00:41 -0400

    Fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function About() {
         I'm an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p class="currently glow">
-        2020 has been a year of reflection for me, and I realied that I really miss building things hands-on.
+        2020 has been a year of reflection for me, and I realized that I really miss building things hands-on.
         I’m currently looking for a remote full-stack software engineering position focused on frontend. Let's talk! 💫
       </p>
       <p class="contact">
