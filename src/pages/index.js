eb2f0fc9a95a47e41f4e1d6817f54deda16e8a84
AGW <agiraldow@gmail.com>
2020-08-22 20:29:06 -0400

    Add CSS gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,19 @@ export default function About() {
         I'm an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
       </p>
       <p class="currently glow">
-        2020 has been a year of reflection for me, and I really miss building things hands-on.
+        2020 has been a year of reflection for me, and I realied that I really miss building things hands-on.
         I’m currently looking for a remote full-stack software engineering position focused on frontend. Let's talk! 💫
       </p>
       <p class="contact">
         Get in touch via <a href="mailto:agiraldow@gmail.com">email</a>. You can find me on <a href="http://twitter.com/awanderingorill" target="_blank">Twitter</a> and <a href="http://github.com/awanderingorill" target="_blank">GitHub</a>. I also post 🎵 on <a href="http://soundcloud.com/verita-gomez" target="_blank">SoundCloud</a>  and 📷 on <a href="http://instagram.com/anita.gw" target="_blank">Instagram</a>.
       </p>
-      {/* <p class="mushrooms">
-        <div class="mobile-description">Here is a photo I took of a cool mushroom, for good measure.</div>
-        <img src="https://instagram.fewr1-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/117133712_3143536909055545_8441520914769024663_n.jpg?_nc_ht=instagram.fewr1-6.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qUA6uwe26fAAX8DOsGj&oh=e42e146c540624089cf6618960d57692&oe=5F675AAB"></img>
-        <div class="description">Here is a photo I took of a cool mushroom, for good measure.</div>
-      </p> */}
+      <p class="mushrooms">
+        <div class="mobile-description">I've been really into making CSS gradients lately. I'm not sure what I will use them for yet, but this is one of them.</div>
+        <div class="gradient-background">
+          <div class="gradient-foreground"></div>
+        </div>
+        <div class="description">I've been really into making CSS gradients lately. I'm not sure what I will use them for yet, but this is one of them!</div>
+      </p>
     </Container>
   )
 }
