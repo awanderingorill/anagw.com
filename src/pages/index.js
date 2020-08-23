826f1e1623f874dd8c00250b72fe239c26402cee
AGW <agiraldow@gmail.com>
2020-08-23 13:21:26 -0400

    Reduce font size on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default () =>
     <Layout></Layout>
     <Container>
           <h1>Ana Giraldo-Wingler</h1>
-          <p class="story">
+          <p class="about">
             I'm an internet-bred hybrid. From 2014-2018, I worked as a software developer doing full-stack work, mostly frontend. During that time, I co-founded an art-tech agency called <a href="http://girlfriends.site" target="new">Girlfriends Labs</a>. Since 2018, I have worked as a Product Manager, first at <a href="http://siberia.io" target="new">Siberia</a> and then at the <a href="http://drl.io" target="new">Drone Racing League</a>.
           </p>
           <p class="currently glow">
