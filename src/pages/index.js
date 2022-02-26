057f3e866908381c7ceda8250e23c39be64612f3
AGW <agiraldow@gmail.com>
2022-02-25 22:56:57 -0500

    Update website to 2022

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,26 @@ export default () =>
   <>
     <Layout></Layout>
     <Container>
-      <div class="face-plate">
-        <div class="first-column">
-          Ana Giraldo-Wingler—Product Manager, Software Developer, and Designer.
-        </div>
-        <div class="second-column">
-          <div class="gradient">
-              <div class="description">
-              </div>
-              <div class="gradient-background">
-                <div class="gradient-foreground"></div>
-              </div>
-            </div>
-        </div>
+      <div class="header-wrapper">
+        <h1>Ana Giraldo-Wingler</h1>
+        <img src="./img/IMG_8320-V2.jpg" alt="Ana Giraldo-Wingler"></img>
       </div>
+      <h2>
+      A complex she/her. I recently moved to Philadelphia and was in NYC for eight years before that. I’m half Colombian and half gringa—lived in Bogotá until the tender age of 10, and go back often. I spent the rest of my childhood in central New Jersey in the ‘00s, still an emo kid at heart. Now I work, I read tarot, and I make music. I’m a Capricorn, so let’s start with work.      </h2>
+      
+      <p>
+        <span>Work</span>
+      I’m a Staff Product Manager at Walmart on a team that de-risks potentially high value initiatives through experimentation and prototyping. The intersection of Agile software development, lean methodologies, and design thinking fascinates me. No, actually. I nerd out on this stuff. I love helping others be productive and balancing customer needs/business requirements—always backed by data (qual and quant!). I’ll never not be on my soapbox about UX research and love a good design studio.
+      </p>
+
+      <p>
+        <span>Tarot</span>
+      I’m also an experienced tarot reader. To me, the tarot is a tool that helps bring into focus things we often already knew but hadn’t acknowledged or noticed we knew. I offer 30-minute and one-hour virtual readings, and they’re totally worth it. You can book me either through Calendly, or on the Ritual Shoppe website.
+      </p>
+
+      <p>
+        <span>Music</span>
+      After reading Artist’s Way, I started making electronic music, mostly in Ableton. A lot of it is in Spanish, but I recently made a song in English and am excited to explore that more (even though it feels more vulnerable somehow). Check me out on SoundCloud. I love collabs and have done a fair amount of them both via Zoom and IRL, so if you want to make music together, please get in touch!
+      </p>
     </Container>
   </>
