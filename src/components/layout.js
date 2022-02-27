1156ee3becf5fbca1a726fd36167264a09638b41
AGW <agiraldow@gmail.com>
2022-02-27 12:14:42 -0500

    Update site thumbnail

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Helmet from "react-helmet"
 export default ({ children }) => (
     <>
       <Helmet>
+        <link rel="image_src" href="./img/IMG_8320-V2.jpg" />
         <title>Ana Giraldo-Wingler</title>
       </Helmet>
     </>
